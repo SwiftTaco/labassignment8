224e2facd04ca1bd2a401bf959c4cea5a2780b20
SwiftTaco <sapito12@yahoo.com>
2024-03-14 22:47:44 -0400

    commit

--- sort.c
+++ sort.c
@@ -29,8 +29,73 @@ size_t Size(void* ptr)
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+
+void merge(int pData[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int *L = (int*)Alloc(n1 * sizeof(int));
+	int *R = (int*)Alloc(n2 * sizeof(int));
+
+	for(i = 0; i < n1; i++)
+	{
+		L[i] = pData[l + i];
+	}
+	for(j = 0; j < n2; j++)
+	{
+		R[j] = pData[m + 1 + j];
+	}
+	
+	i = 0;
+	j = 0;
+	k = l;
+
+	while(i < n1 && j < n2)
+	{
+		if(L[i] <= R[j])
+		{
+			pData[k] = L[i];
+			i++;
+		}
+		else
+		{
+			pData[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i < n1)
+	{
+		pData[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while(j < n2)
+	{
+		pData[k] = R[j];
+		j++;
+		k++;
+	}
+	DeAlloc(L);
+	DeAlloc(R);
+	
+}
+
 void mergeSort(int pData[], int l, int r)
 {
+	if(l < r)
+	{
+		int m = l + (r - l)/2;
+
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+		
+		merge(pData, l, m, r);
+	}
 }
 
 // parses input file to an integer array
@@ -67,9 +132,10 @@ int parseData(char *inputFileName, int **ppData)
 // prints first and last 100 items in the data array
 void printArray(int pData[], int dataSz)
 {
-	int i, sz = dataSz - 100;
+	int i, sz = (dataSz > 100 ? dataSz - 100 : 0);
+	int firstHundred = (dataSz < 100 ? dataSz : 100);
 	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
+	for (i=0;i<firstHundred;++i)
 	{
 		printf("%d ",pData[i]);
 	}
